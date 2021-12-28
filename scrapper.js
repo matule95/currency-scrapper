413b2e7132b795ff966b4fb5d497fec05a69d3e3
Clayton Matule <clayton.matule@hotmail.com>
2021-12-28 14:20:02 +0200

    feat: added node-cron dependency to project

--- scrapper.js
+++ scrapper.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
+const cronJob = require("node-cron");
 const logger = require("./logger");
 logger.dispatch("Initializing Currency Scrapper");
 const browserObject = require("./browser");
 const scraperController = require("./pageController");
 let browserInstance = browserObject.startBrowser();
 module.exports = {
-  // initializeScrapper: () =>
-  //   setInterval(() => scraperController(browserInstance), 60000),
-  initializeScrapper: () =>
-    setInterval(() => scraperController(browserInstance), 60000),
+  // initializeScrapper: () => scraperController(browserInstance),
+  initializeScrapper: () => {
+    cronJob.schedule("* * * * *", function () {
+      scraperController(browserInstance);
+    });
+  },
 };
